c75652137997dff6c32595fb68ce69a709fae547
Lee <l783l74@hotmail.com>
2019-11-05 16:31:17 +0800

    add index.html

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Why so easy</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<username>')
 def show(username):
     return f"Hi {username}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
