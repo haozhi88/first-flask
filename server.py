65e7bd877674405bcd346b34822f71d5677237b7
Lee <l783l74@hotmail.com>
2019-11-05 16:40:10 +0800

    add profile.html

--- server.py
+++ server.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username}"
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template('profile.html', name=name)
+
+@app.route('/user/<name>')
+def show(name):
+    return f"Hi {name}"
 
 if __name__ == '__main__':
     app.run(debug=True)
