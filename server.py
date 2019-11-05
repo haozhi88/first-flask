9a188b013d9f8b33f2819c34f0a643c05e0901b3
Lee <l783l74@hotmail.com>
2019-11-05 17:11:10 +0800

    just update

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+signed_in = False
 
 @app.route("/")
 def index():
-    signed_in = False
     return render_template('index.html', signed_in=signed_in)
 
 @app.route('/profile/<name>')
 def profile(name):
-    return render_template('profile.html', name=name)
+    return render_template('profile.html', name=name, signed_in=signed_in)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', signed_in=signed_in)
 
 @app.route('/user/<name>')
 def show(name):
