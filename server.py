b64fa0c15d63a9cc4bb3bce7c41143918b987cc0
Lee <l783l74@hotmail.com>
2019-11-05 17:42:23 +0800

    add _layout and others

--- server.py
+++ server.py
@@ -8,11 +8,16 @@ def index():
 
 @app.route('/profile/<name>')
 def profile(name):
-    return render_template('profile.html', name=name, signed_in=signed_in)
+    num = len(name)
+    return render_template('profile.html', name=name, signed_in=signed_in, num=num)
 
 @app.route('/login')
 def login():
     return render_template('login.html', signed_in=signed_in)
+
+@app.route('/layout')
+def layout():
+    return render_template('extend_layout.html')
 
 @app.route('/user/<name>')
 def show(name):
